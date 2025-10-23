aa17c285f538fb54403872cff34b50d593240586
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-10-23 21:58:56 +0600

    image and font optimaization

--- app/about/mission/page.js
+++ app/about/mission/page.js
@@ -1,11 +1,15 @@
 
 import Button from '@/app/components/Button'
+import Image from 'next/image'
 import React from 'react'
+import natureImage from '@/public/nature.jpg'
 
 const page = () => {
   return (
     <div className='text-blue-500 mt-10'>
       this is mission page .and her  parent page about
+
+      <Image src={natureImage} alt='nature image' ></Image>
       
       <Button></Button>
     </div>
--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,17 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Roboto } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const roboto = Roboto({
+  weight: "400",
   subsets: ["latin"],
 });
 
@@ -20,7 +23,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={roboto.className}>
         <nav className=" text-green-500">
           <ul className="flex gap-6">
             <li>
